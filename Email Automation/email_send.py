2169a1b5dd7d7ad9bd66368a990fcbe2d6720305
Khalid Rehman <115589136+khalidrehman8221@users.noreply.github.com>
2023-04-09 19:05:09 +0500

    Update email_send.py

--- Email Automation/email_send.py
+++ Email Automation/email_send.py
@@ -6,10 +6,10 @@ to = input("Enter the email address of the reciver :-")
 message = input("Enter the message :-")
 
 def sendEmail(to, message);
-   sever = smtplib.SMTP('smtp.gamil.com',587)
-   sever.starttls()
-   sever.login('senderemail','password')
+   server = smtplib.SMTP('smtp.gamil.com',587)
+   server.starttls()
+   server.login('senderemail','password')
    server.sendmail(senderemail, to, message)
-   sever.close()
+   server.close()
 
 sendEmail(to, message)  
